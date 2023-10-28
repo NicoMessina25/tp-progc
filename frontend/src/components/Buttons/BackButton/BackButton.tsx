603bc48f229635eecd290409828d0fa59ca3dd62
NicoMessina25 <nicolasmessina25@gmail.com>
2023-10-28 16:27:32 -0300

    fix BackButton

--- frontend/src/components/Buttons/BackButton/BackButton.tsx
+++ frontend/src/components/Buttons/BackButton/BackButton.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 
 
 
-export default function BackButton({className = "", text = "Volver", onClick}:ButtonProps) {
+export default function BackButton({className = "", text = "Back", onClick}:ButtonProps) {
     const router = useRouter();
 
     return <Button type='button' variant={'outline'} className={className} onClick={onClick ?? router.back} ><Icon className='mr-1' icon={"icon-park-solid:back"} /> {text}  </Button>
